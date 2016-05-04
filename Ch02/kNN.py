28c9ff2733edca58f8077b67bfe249529126dfa5
bi_admin <bi_admin@wuxiapptec.com>
2016-05-04 17:33:54 +0800

    no message

--- Ch02/kNN.py
+++ Ch02/kNN.py
@@ -1,4 +1,4 @@
-'''
+"""
 Created on Sep 16, 2010
 Updated on Mar 3, 2016
 kNN: k Nearest Neighbors
@@ -12,138 +12,160 @@ Output:     the most popular class label
 
 @author: pbharrin
 @update: pan_junjun
-'''
-from numpy import *
+"""
 import operator
 from os import listdir
-from settings import debug
+
+from numpy import *
+
+from settings import debug, DEBUG
 
 
-def classify0(input_x, data_set, labels, k):
+def classify0(input_x, data_set, labels, k_value):
     data_set_size = data_set.shape[0]
-    debug("data_set_size=%s" % data_set_size)
+    debug(data_set_size)
     diff_matrix = tile(input_x, (data_set_size, 1)) - data_set
-    debug("diff_matrix=%s" % diff_matrix)
+    debug(diff_matrix)
     square_diff_matrix = diff_matrix ** 2
-    debug("square_diff_matrix=%s" % square_diff_matrix)
+    debug(square_diff_matrix)
     square_distance = square_diff_matrix.sum(axis=1)
-    debug("square_distance=%s" % square_distance)
+    debug(square_distance)
     distances = square_distance ** 0.5
-    debug("distances=%s" % distances)
+    debug(distances)
     sorted_dist_index = distances.argsort()
-    debug("sorted_dist_index=%s" % sorted_dist_index)
+    debug(sorted_dist_index)
     class_count = {}
-    for i in range(k):
-        debug("i=%s" % i)
+    for i in range(k_value):
+        debug(i)
         vote_i_label = labels[sorted_dist_index[i]]
-        debug("vote_i_label=%s" % vote_i_label)
+        debug(vote_i_label)
         class_count[vote_i_label] = class_count.get(vote_i_label, 0) + 1
-        debug("class_count[vote_i_label]=%s" % class_count[vote_i_label])
-    debug("loop end")
+        debug(class_count[vote_i_label])
+    print("loop end")
     sorted_class_count = sorted(class_count.iteritems(),
-                                key=operator.itemgetter(1), reverse=True)
-    debug("sorted_class_count=%s" % sorted_class_count)
+        key=operator.itemgetter(1), reverse=True)
+    debug(sorted_class_count)
     return sorted_class_count[0][0]
 
 
 def create_data_set():
     group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
-    labels = ['A', 'A', 'B', 'B']
+    labels = ["A", "A", "B", "B"]
     return group, labels
 
 
 def file2matrix(filename):
     fr = open(filename)
     array_of_lines = fr.readlines()
-    number_of_lines = len(array_of_lines)         #get the number of lines in the file
-    debug("number_of_lines=%s" % number_of_lines)
-    return_matrix = zeros((number_of_lines, 3))        #prepare matrix to return
-    debug("return_matrix=%s" % return_matrix)
-    class_label_vector = []                       #prepare labels return
+    number_of_lines = len(array_of_lines)  # get the number of lines in the file
+    debug(number_of_lines)
+    return_matrix = zeros((number_of_lines, 3))  # prepare matrix to return
+    debug(return_matrix)
+    class_label_vector = []  # prepare labels return
     index = 0
     for line in array_of_lines:
-        debug("index=%s" % index)
-        debug("line=%s" % line)
+        debug(index)
+        debug(line)
         line = line.strip()
-        list_from_line = line.split('\t')
+        list_from_line = line.split("\t")
         return_matrix[index, :] = list_from_line[0: 3]
-        class_label_vector.append(int(list_from_line[-1]))
+        class_label_vector.append(int(list_from_line[-1]))  # only for the int element
         index += 1
-    debug("loop end")
+    print("loop end")
     return return_matrix, class_label_vector
 
 
-def autoNorm(data_set):
-    minVals = data_set.min(0)
-    maxVals = data_set.max(0)
-    ranges = maxVals - minVals
-    normdata_set = zeros(shape(data_set))
+def auto_normalization(data_set):
+    min_values = data_set.min(0)
+    debug(min_values)
+    max_values = data_set.max(0)
+    debug(max_values)
+    ranges = max_values - min_values
+
+    # norm_data_set = zeros(shape(data_set))
     m = data_set.shape[0]
-    normdata_set = data_set - tile(minVals, (m,1))
-    normdata_set = normdata_set/tile(ranges, (m,1))   #element wise divide
-    return normdata_set, ranges, minVals
+    norm_data_set = data_set - tile(min_values, (m, 1))
+    norm_data_set = norm_data_set / tile(ranges, (m, 1))  # element wise divide
+    return norm_data_set, ranges, min_values
 
 
-def datingClassTest():
-    hoRatio = 0.50      #hold out 10%
-    datingDataMat,datingLabels = file2matrix('datingTestSet2.txt')       #load data setfrom file
-    normMat, ranges, minVals = autoNorm(datingDataMat)
-    m = normMat.shape[0]
-    numTestVecs = int(m*hoRatio)
-    errorCount = 0.0
-    for i in range(numTestVecs):
-        classifierResult = classify0(normMat[i,:],normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],3)
-        print("the classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i]))
-        if (classifierResult != datingLabels[i]): errorCount += 1.0
-    print("the total error rate is: %f" % (errorCount/float(numTestVecs)))
-    print(errorCount)
+def dating_class_test():
+    ho_ratio = 0.50  # hold out 10%
+    dating_data_matrix, dating_labels = file2matrix("datingTestSet2.txt")  # load data setfrom file
+    norm_matrix, ranges, min_values = auto_normalization(dating_data_matrix)
+    m = norm_matrix.shape[0]
+    numeric_test_vectors = int(m * ho_ratio)
+    error_count = 0.0
+    for i in range(numeric_test_vectors):
+        classifier_result = classify0(norm_matrix[i, :], norm_matrix[numeric_test_vectors:m, :],
+                                      dating_labels[numeric_test_vectors:m], 3)
+        print("the classifier came back with: %d, the real answer is: %d" % (classifier_result, dating_labels[i]))
+        if classifier_result != dating_labels[i]:
+            error_count += 1.0
+    print("the total error rate is: %f" % (error_count / float(numeric_test_vectors)))
+    print(error_count)
 
 
 def img2vector(filename):
-    returnVect = zeros((1,1024))
+    returnVect = zeros((1, 1024))
     fr = open(filename)
     for i in range(32):
         lineStr = fr.readline()
         for j in range(32):
-            returnVect[0,32*i+j] = int(lineStr[j])
+            returnVect[0, 32 * i + j] = int(lineStr[j])
     return returnVect
 
 
 def handwritingClassTest():
     hwLabels = []
-    trainingFileList = listdir('trainingDigits')           #load the training set
+    trainingFileList = listdir("trainingDigits")  # load the training set
     m = len(trainingFileList)
-    trainingMat = zeros((m,1024))
+    trainingMat = zeros((m, 1024))
     for i in range(m):
         fileNameStr = trainingFileList[i]
-        fileStr = fileNameStr.split('.')[0]     #take off .txt
-        classNumStr = int(fileStr.split('_')[0])
+        fileStr = fileNameStr.split(".")[0]  # take off .txt
+        classNumStr = int(fileStr.split("_")[0])
         hwLabels.append(classNumStr)
-        trainingMat[i,:] = img2vector('trainingDigits/%s' % fileNameStr)
-    testFileList = listdir('testDigits')        #iterate through the test set
-    errorCount = 0.0
+        trainingMat[i, :] = img2vector("trainingDigits/%s" % fileNameStr)
+    testFileList = listdir("testDigits")  # iterate through the test set
+    error_count = 0.0
     mTest = len(testFileList)
     for i in range(mTest):
         fileNameStr = testFileList[i]
-        fileStr = fileNameStr.split('.')[0]     #take off .txt
-        classNumStr = int(fileStr.split('_')[0])
-        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
-        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
-        print("the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr))
-        if (classifierResult != classNumStr): errorCount += 1.0
-    print("\nthe total number of errors is: %d" % errorCount)
-    print("\nthe total error rate is: %f" % (errorCount/float(mTest)))
+        fileStr = fileNameStr.split(".")[0]  # take off .txt
+        classNumStr = int(fileStr.split("_")[0])
+        vectorUnderTest = img2vector("testDigits/%s" % fileNameStr)
+        classifier_result = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        print("the classifier came back with: %d, the real answer is: %d" % (classifier_result, classNumStr))
+        if (classifier_result != classNumStr):
+            error_count += 1.0
+    print("\nthe total number of errors is: %d" % error_count)
+    print("\nthe total error rate is: %f" % (error_count / float(mTest)))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     ####### create_data_set test #######
-    #data_set, label = create_data_set()
-    #debug(data_set)
-    #debug(label)
+    # data_set, label = create_data_set()
+    # debug(data_set)
+    # debug(label)
     ####### classify0 test #######
-    #classified = classify0([0.0, 0.5], data_set, label, 3)
+    # classified = classify0([0.0, 0.5], data_set, label, 3)
     #print classified
     ####### file2matrix test #######
-    dating_data_matrix, dating_labels = file2matrix('datingTestSet2.txt')
-    #print dating_data_matrix.shape
+    dating_data_mat, dating_label = file2matrix("datingTestSet2.txt")
+    debug("dating_data_mat=%s" % dating_data_mat)
+    debug("dating_label[0:20]=%s" % dating_label[0:20])
+    if dating_data_mat.any() and dating_label and DEBUG:
+        import matplotlib.pyplot as plt
 
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.scatter(dating_data_mat[:, 2], dating_data_mat[:, 0], 15.0 * array(dating_label), 15.0 * array(dating_label))
+        plt.show()
+
+    norm_mat, value_range, min_value = auto_normalization(dating_data_mat)
+    debug("norm_mat=%s" % norm_mat)
+    debug("value_range=%s" % value_range)
+    debug("min_value=%s" % min_value)
+
+    #print dating_data_matrix.shape
--- Ch02/settings.py
+++ Ch02/settings.py
@@ -1,13 +1,25 @@
 DEBUG = True
 
 
-def debug(string):
+def debug(variable_object):
     if DEBUG:
-        print string
+        #print "globals()=%s" % globals
+        variable_name = None
+        for k, v in globals().items():
+            if v is variable_object:
+                variable_name = k
+                #print variable_name
+        if variable_name:
+            print "%s=%s" % (variable_name, variable_object)
+        else:
+            print variable_object
 
 
 if __name__ == '__main__':
+    c = {"a": 1, "b": 2}
     DEBUG = True
-    debug('debug = True')
+    debug(DEBUG)
+    debug(c)
     DEBUG = False
-    debug('debug = False')
+    debug(DEBUG)
+
